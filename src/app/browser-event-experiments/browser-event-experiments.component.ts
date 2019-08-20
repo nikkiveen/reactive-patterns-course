dd77d96154cd46c3242986fe186a21fee10192b5
Nikki Veen <nikki@otus.com>
2019-08-20 17:46:06 -0500

    subscribe and unsubscribe to browser events using add/removeEventListener to demonstrate the similarities to reactive programming

--- src/app/browser-event-experiments/browser-event-experiments.component.ts
+++ src/app/browser-event-experiments/browser-event-experiments.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'browser-event-experiments',
-  templateUrl: './browser-event-experiments.component.html',
-  styleUrls: ['./browser-event-experiments.component.scss']
+    selector: 'browser-event-experiments',
+    templateUrl: './browser-event-experiments.component.html',
+    styleUrls: ['./browser-event-experiments.component.scss']
 })
 export class BrowserEventExperimentsComponent implements OnInit {
+    hoverSection: HTMLElement;
 
-  constructor() { }
+    ngOnInit() {
+        this.hoverSection = document.getElementById('hover');
 
-  ngOnInit() {
-  }
+        this.hoverSection.addEventListener('mousemove', onMouseMove);
+    }
 
+    unsubscribe() {
+        console.log('Called unsubscribe()');
+        this.hoverSection.removeEventListener('mousemove', onMouseMove);
+    }
+}
+
+function onMouseMove(ev: MouseEvent) {
+    console.log(ev);
 }
