c9f703b61524b1d66031b9818d5e40d9f4e39241
Nikki Veen <nikki@otus.com>
2019-08-22 14:30:23 -0500

    setup event bus in lessons list component

--- src/app/event-bus-experiments/event-bus-experiments.component.ts
+++ src/app/event-bus-experiments/event-bus-experiments.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { globalEventBus } from './event-bus';
+import { testLessons } from '../shared/models/test-lessons';
 
 @Component({
-  selector: 'event-bus-experiments',
-  templateUrl: './event-bus-experiments.component.html',
-  styleUrls: ['./event-bus-experiments.component.scss']
+    selector: 'event-bus-experiments',
+    templateUrl: './event-bus-experiments.component.html',
+    styleUrls: ['./event-bus-experiments.component.scss']
 })
 export class EventBusExperimentsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+    ngOnInit() {
+        console.log("EventBusExperimentsComponent broadcasted all lessons ...");
+        globalEventBus.notifyObservers(testLessons);
+    }
 }
--- src/app/lessons-list/lessons-list.component.ts
+++ src/app/lessons-list/lessons-list.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { globalEventBus, Observer } from '../event-bus-experiments/event-bus';
+import { Lesson } from '../shared/models/lesson';
 
 @Component({
-  selector: 'lessons-list',
-  templateUrl: './lessons-list.component.html',
-  styleUrls: ['./lessons-list.component.scss']
+    selector: 'lessons-list',
+    templateUrl: './lessons-list.component.html',
+    styleUrls: ['./lessons-list.component.scss']
 })
-export class LessonsListComponent implements OnInit {
+export class LessonsListComponent implements Observer {
+    lessons: Lesson[] = [];
 
-  constructor() { }
+    constructor() {
+        console.log("LessonsListComponent registered is registered as observer ...");
+        globalEventBus.registerObserver(this);
+    }
 
-  ngOnInit() {
-  }
-
+    notify(data: Lesson[]) {
+        console.log("LessonsListComponent received data ...");
+        this.lessons = data;
+    }
 }
