cd8fdf8b8769fd69477f4a1e2f6704151e49fbf7
Nikki Veen <nikki@otus.com>
2019-08-22 13:46:44 -0500

    setup for event bus experiments

--- src/app/event-bus-experiments/event-bus-experiments.component.ts
+++ src/app/event-bus-experiments/event-bus-experiments.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'event-bus-experiments',
   templateUrl: './event-bus-experiments.component.html',
-  styleUrls: ['./event-bus-experiments.component.css']
+  styleUrls: ['./event-bus-experiments.component.scss']
 })
 export class EventBusExperimentsComponent implements OnInit {
 
--- src/app/lessons-list/lessons-list.component.ts
+++ src/app/lessons-list/lessons-list.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'lessons-list',
   templateUrl: './lessons-list.component.html',
-  styleUrls: ['./lessons-list.component.css']
+  styleUrls: ['./lessons-list.component.scss']
 })
 export class LessonsListComponent implements OnInit {
 
