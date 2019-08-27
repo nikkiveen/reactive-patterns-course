53e62570dc101af9c8bbd69d5a9c65288eea4f61
Nikki Veen <nikki@otus.com>
2019-08-26 22:08:32 -0500

    demonstrate how event bus is not sustainable because any component can trigger any type of event and there is no true owner of the data

--- src/app/event-bus-experiments/event-bus-experiments.component.ts
+++ src/app/event-bus-experiments/event-bus-experiments.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { globalEventBus, LESSONS_LIST_AVAILABLE, ADD_NEW_LESSON } from './event-bus';
 import { testLessons } from '../shared/models/test-lessons';
+import { Lesson } from '../shared/models/lesson';
 
 @Component({
     selector: 'event-bus-experiments',
@@ -8,10 +9,21 @@ import { testLessons } from '../shared/models/test-lessons';
     styleUrls: ['./event-bus-experiments.component.scss']
 })
 export class EventBusExperimentsComponent implements OnInit {
+    lessons: Lesson[] = [];
+
     ngOnInit() {
         console.log("EventBusExperimentsComponent broadcasted all lessons ...");
-        globalEventBus.notifyObservers(LESSONS_LIST_AVAILABLE, testLessons);
-        testLessons.slice(0);
+        this.lessons = testLessons.slice(0);
+        globalEventBus.notifyObservers(LESSONS_LIST_AVAILABLE, this.lessons);
+
+        setTimeout(() => {
+            this.lessons.push({
+                id: Math.random(),
+                description: "New Lesson Arriving From the Backend"
+            });
+
+            globalEventBus.notifyObservers(LESSONS_LIST_AVAILABLE, this.lessons);
+        }, 10000);
     }
 
     addLesson(lessonText: string) {
